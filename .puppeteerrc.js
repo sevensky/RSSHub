cbcf413414a3185cae2b79c997079a12c37d44a4
tickup <tickup@qq.com>
2024-11-25 04:57:23 +0000

    s

--- .puppeteerrc.js
+++ .puppeteerrc.js
@@ -6,14 +6,15 @@ const { join } = require('path');
 module.exports = {
     // Changes the cache location for Puppeteer.
     cacheDirectory: join(__dirname, 'node_modules', '.cache', 'puppeteer'),
+
     // "chrome-headless-shell"
     // executablePath
     // Download Chrome (default `skipDownload: false`).
-    chrome: {
-        skipDownload: false,
-    },
-    // Download Firefox (default `skipDownload: true`).
-    firefox: {
-        skipDownload: true,
-    },
+    // chrome: {
+    //     skipDownload: false,
+    // },
+    // // Download Firefox (default `skipDownload: true`).
+    // firefox: {
+    //     skipDownload: true,
+    // },
 };
